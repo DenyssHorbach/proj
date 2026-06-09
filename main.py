51fa8a638299266124e8d713e1e2f0f442633442
Denyss <gorbachdenis200@gmail.com>
2026-06-09 12:08:44 +0300

    added doublyLinkedList, methods: append, insert,__getitem__, print_list

---
+++ main.py
@@ -0,0 +1,14 @@
+from classes.DoublyLinkedList import *
+
+
+test = DoublyLinkedList()
+
+test.append(24)
+test.append(14)
+test.append(4)
+
+test.print_list()
+
+test.insert(0, 25)
+
+test.print_list()
